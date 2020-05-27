d846f911061769c1506ed3f1ba4b8f7bf7cea71f
xlqwyd <xlqwyd3@gmail.com>
2020-05-27 16:51:03 +0900

    test123123브랜치

---
+++ hihi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+int a=5;
+int b=2;
+int c=3;
+printf("a(%d)와 b(%d) 의 합계 : %d 입니다.",a,b,a+b);
+return 0;
+}
---
+++ test333.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+int a=5000;
+int b=1112;
+
+printf("a(%d)와 b(%d) 의 합계 : %d 입니다.",a,b,a+b);
+return 0;
+}
